eb59d270dae2eafc97deeca3a121a4ad9708f8c7
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-25 02:48:16 +0400

    Create MovieCard.jsx

---
+++ movie-app/src/components/MovieCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MovieCard = ({movie: { title, vote_average, poster_path, original_language}}) => {
+  return (
+    <div className='movie-card'>
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title} />
+      <div className='mt-4'>
+        <h3>{title}</h3>
+        <div className='content'>
+            <div className='rating'>
+                <img src="rating.svg" alt="Star Icon" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
