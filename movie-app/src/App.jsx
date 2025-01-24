ef696f4d7d9a0e1766eface81dd5b15200299988
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-25 02:48:14 +0400

    Update App.jsx

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search.jsx"
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3"
 
@@ -66,15 +68,15 @@ const App = () => {
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
           <section className="all-movies">
-            <h2>All Movies</h2>
+            <h2 className="mt-[40px]">All Movies</h2>
             {isLoading ? (
-              <p className="text-white">Loading...</p>
+              <Spinner />
             ) : errorMessage ? (
               <p className="text-red-500">{errorMessage}</p>
             ) : (
               <ul>
                 {movies.map((movie) => (
-                  <p key={movie.id} className="text-white">{movie.title}</p>
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
               </ul>
             )}
