56d2d060418b0925815c86dd05bfc0cf6dfe7c55
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-25 11:52:14 +0400

    Update App.jsx

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -21,12 +21,12 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query="") => {
     setIsLoading(true);
     setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint,API_OPTIONS);
 
       if (!response.ok) {
@@ -52,8 +52,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
 
   return (
